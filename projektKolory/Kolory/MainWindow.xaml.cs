da82958ef9fa16a49213866cc598058e94fae7a8
Adam Horo <loginadas@gmail.com>
2025-10-04 09:42:46 +0200

    projekt kolory

---
+++ projektKolory/Kolory/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kolory
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Color rectangleColor
+        {
+            get
+            {
+                return (rectangle.Fill as SolidColorBrush).Color;
+            }
+            set
+            {
+                (rectangle.Fill as SolidColorBrush).Color = value;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Settings.LoadWindow(this);
+            Color color = Settings.LoadColor();
+            rectangle.Fill = new SolidColorBrush(color);
+            sliderR.Value = color.R;
+            sliderG.Value = color.G;
+            sliderB.Value = color.B;
+        }
+
+        private void sliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Color color = Color.FromRgb((byte)sliderR.Value, (byte)sliderG.Value, (byte)sliderB.Value);
+            rectangleColor = color;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) 
+            { 
+                Close();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+                Settings.SaveColor(rectangleColor);
+                Settings.SaveWindow(this);
+        }
+    }
+}
---
+++ projektKolory/Kolory/Settings.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Kolory
+{
+    internal class Settings
+    {
+        public static Color LoadColor()
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+            Color color = new Color()
+            {
+                R = settings.R,
+                G = settings.G,
+                B = settings.B,
+            };
+            return color;
+        }
+
+        public static void SaveColor(Color color)
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+            settings.R = color.R;
+            settings.G = color.G;
+            settings.B = color.B;
+            settings.Save();
+        }
+
+        public static void LoadWindow(Window window)
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+            window.Left = settings.x;
+            window.Top = settings.y;
+            window.Width = settings.width;
+            window.Height = settings.height;
+        }
+
+        public static void SaveWindow(Window window)
+        {
+            Properties.Settings settings = Properties.Settings.Default;
+            settings.x = window.Left;
+            settings.y = window.Top;
+            settings.width = window.Width;
+            settings.height = window.Height;
+            settings.Save();
+        }
+    }
+}
